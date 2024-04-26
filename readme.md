fea9c1087f33c21cff32768d9a48d53d184170b1
DeadLeavez <DeadLeavez@users.noreply.github.com>
2024-04-26 02:50:19 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,49 @@
+# Progressive Container
+Adds crafts for progressively getting a bigger container.
+
+The recipes are quite expensive by default. It's simply the way I prefer things. Can be changed in the config file.
+
+the crafts are as following by default.
+
+```
+pouch: 4xbolts, 4xnuts, pistol case, 2xduct tape, 2xblue tape
+alpha: 2xpouch, 4xmetal parts, 2xkektape, magazine case, 2xpoexeram
+beta:  2xalpha, 4xscrews, 4xnails, ammo case, bit coin
+epsi:  2xbeta, virtex, scav junk box, key tool, 2xgraphics card, shturman key, golden tt, sledge hammer
+gamma: 2xepsi, Tagilla cap, Sanitar Bag, Killa Helmet, 1xweapon case, 2xAESA, yellow keycard, tank battery
+kappa: 2xgamma, 4xbig pipe pipe, 2xaspect company key, 2 x far foward, red keycard, violet keycard, blue keycard
+```
+
+I personally run with these container sizes.
+
+```
+pouch: 2x3
+alpha: 3x3
+beta:  3x4
+epsi:  3x5
+gamma: 3x6
+kappa: 3x7
+```
+
+You need another mod (like SVM) to edit container sizes.
+
+Inspired by part of Trap's progressive stash, but no code has been copied. Still, huge thanks for the amazing idea!
+
+# Changelog
+## 1.0.0
+Initial Release
+
+## 1.1.0
+- Fixed a bug where crafts that were left going after shutting the server down could cause the craft to disappear. 
+- also fixed the issue of favorite crafts disappearing.
+
+- Also added a feature that allow you to remove the alpha/beta containers from peacekeeper. (Enabled by default)
+
+
+If you have messages like ``Could not find recipe: <ID> for area type`` they are caused by the aforementioned bug. It is completely harmless, but if you want to remove the messages. remove crafting entries in the ``profile.json`` matching the ID the message is outputting. I HIGHLY RECOMMEND YOU NOT TO DO THAT BECAUSE IF DONE IMPROPERLY YOU CAN AND WILL PERMANENTLY BRICK YOUR PROFILE.
+
+
+## 1.2.0
+
+- Updated version for 3.8.0
+- Adjusted the config file to new format that allows comments for easier editing.
---
+++ src/mod.ts
@@ -0,0 +1,134 @@
+import { DependencyContainer } from "tsyringe";
+import { Ilogger } from "@spt-aki/models/spt/utils/Ilogger";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
+import { VFS } from "@spt-aki/utils/VFS";
+import { jsonc } from "jsonc";
+import * as path from "path";
+import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
+
+class ProgressiveContainer implements IPostDBLoadMod
+{
+    private logger: Ilogger;
+    private db: DatabaseServer;
+
+    private craftprefx = "LeavesCraft_ProgressiveContainer"
+
+    private containers = [];
+    private crafts;
+
+    //Config
+    private config: any;
+    private vfs: VFS;
+
+    private createCraft ( container: string ): any
+    {
+        if ( this.config.secure_containers[ container ] )
+        {
+            const area = this.getAreaID( container );
+            this.printColor( "[ProgressiveContainer] \tAdding craft for: " + container, LogTextColor.CYAN );
+            let craftToAdd =
+            {
+                _id: this.craftprefx + container,
+                areaType: area,
+                requirements: this.config.secure_containers[ container ].requirements,
+                productionTime: this.config.craftingTime,
+                needFuelForAllProductionTime: false,
+                locked: false,
+                endProduct: this.containers[ container ],
+                continuous: false,
+                count: 1,
+                productionLimitCount: 0,
+                isEncoded: false
+            }
+
+            return craftToAdd;
+        }
+        return null;
+    }
+    getAreaID ( containerID: string ): number
+    {
+        const container = this.config.secure_containers[ containerID ];
+        {
+            //Find the area ID for the container from the list of requirements.
+            for( const requirement of container.requirements )
+            {
+                if ( requirement.areaType )
+                {
+                    return requirement.areaType;
+                }
+            }
+        }
+        this.printColor( "[ProgressiveContainer] ERROR: Could not find areaType for: " + containerID, LogTextColor.RED );
+        return 10;
+    }
+
+    public postDBLoad ( container: DependencyContainer ): void 
+    {
+        // Get stuff from container
+        this.db = container.resolve<DatabaseServer>( "DatabaseServer" );
+        this.logger = container.resolve<Ilogger>( "WinstonLogger" );
+
+
+        this.vfs = container.resolve<VFS>( "VFS" );
+        const configFile = path.resolve( __dirname, "../config/config.jsonc" );
+        this.config = jsonc.parse( this.vfs.readFile( configFile ) );
+
+        //Crafts List
+        this.crafts = this.db.getTables().hideout.production;
+
+        //IDs for containers
+        this.containers[ "pouch" ] = "5732ee6a24597719ae0c0281";
+        this.containers[ "alpha" ] = "544a11ac4bdc2d470e8b456a";
+        this.containers[ "beta" ] = "5857a8b324597729ab0a0e7d";
+        this.containers[ "epsilon" ] = "59db794186f77448bc595262";
+        this.containers[ "gamma" ] = "5857a8bc2459772bad15db29";
+        this.containers[ "kappa" ] = "5c093ca986f7740a1867ab12";
+
+        this.printColor( "[ProgressiveContainer] ProgressiveContainer Starting:" );
+
+        for ( let container in this.containers )
+        {
+            let craft = this.createCraft( container );
+            this.crafts.push( craft );
+        }
+
+
+        if ( this.config.removeContainersFromPeacekeeper )
+        {
+            this.removeContainerFromPK();
+        }
+    }
+
+    private removeContainerFromPK (): void
+    {
+        const PKID = "5935c25fb3acc3127c3d8cd9";
+        const assort = this.db.getTables().traders[ PKID ].assort.items
+        let toDelete = [];
+
+        for ( let entry = 0; entry < assort.length; entry++ )
+        {
+            if ( assort[ entry ]._tpl === "544a11ac4bdc2d470e8b456a" || assort[ entry ]._tpl === "5857a8b324597729ab0a0e7d" ) 
+            {
+                toDelete.push( entry );
+            }
+        }
+
+        //this.logger.info( "Size before:" + assort.length )
+        let deleteCount = 0;
+        for ( const target of toDelete )
+        {
+            assort.splice( target - deleteCount, 1 );
+            deleteCount++;
+        }
+        //this.logger.info( "Size after:" + assort.length )
+    }
+
+    private printColor ( message: string, color: LogTextColor = LogTextColor.GREEN )
+    {
+        this.logger.logWithColor( message, color );
+    }
+}
+
+module.exports = { mod: new ProgressiveContainer() }
